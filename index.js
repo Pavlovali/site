e3818d66990251283dc11164e487246215146740
Pavlovali <pavlova.li5698@gmail.com>
2022-12-18 12:06:56 +0000

    fk

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-import express from "express";
-import bodyParser from "body-parser"
-const app = express();
-app.use(bodyParser.urlencoded());
+import express from 'express'
+import bodyParser from 'body-parser'
+const app = express()
+const port = process.env.PORT || 8080 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.render('index.ejs');
+      res.render('index.ejs')
+});
+
+app.post('/', (req, res) =>
+    {
+        const username = req.body.username;
+        res.render('hello.ejs', {username: username});
+    });
+
+app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`)
 })
-app.post('/', (req, res) => {
-    const username = req.body.username;
-    res.render('hello.ejs', {username: username})
-})
-const port = process.env.PORT || 8080;
-app.listen(port, ()=>console.log(`listen port: ${port}`))
